0b65020f8ef1edcb6daad4bcef61df468e1d546e
CarlosVerdejo <carlosverdejo77@yahoo.com>
2023-03-31 18:42:44 -0400

    initial code base

---
+++ src/com/skilldistillery/jets/enteties/AirField.java
@@ -0,0 +1,8 @@
+package com.skilldistillery.jets.enteties;
+
+import java.util.List;
+
+public class AirField {
+	private List<Jet> fleet;
+
+}
---
+++ src/com/skilldistillery/jets/enteties/Jet.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.jets.enteties;
+
+public abstract class Jet {
+
+}
